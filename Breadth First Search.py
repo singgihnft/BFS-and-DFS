e51c06eb7cc18409c0dcde43cb02f8d9873038d3
Singgih Nugroho Putro <30440089+singgihnft@users.noreply.github.com>
2019-04-20 22:37:36 +0700

    Tugas Kecerdasan Buatan 1
    Breadth first search (BFS) adalah algoritma yang melakukan pencarian secara melebar yang mengunjungi node secara preorder yaitu mengunjungi suatu node kemudian mengunjungi semua node yang bertetangga dengan node tersebut terlebih dahulu. Selanjutnya, node yang belum dikunjungi dan bertetangga dengan node-node yang tadi dikunjungi , demikian seterusnya. Jika graf berbentuk pohon berakar, maka semua node pada aras d dikunjungi lebih dahulu sebelum node-node pada aras d+1.
    Algoritma ini memerlukan sebuah antrian q untuk menyimpan node yang telah dikunjungi. Node-node ini diperlukan sebagai acuan untuk mengunjungi node-node yang bertetanggaan dengannya. Tiap node yang telah dikunjungi masuk ke dalam antrian hanya satu kali. 
    Sedangkan  Depth First Search (DFS) adalah salah satu algoritma penelusuran struktur graf / pohon berdasarkan kedalaman. Node ditelusuri dari root kemudian ke salah satu node anaknya ( misalnya prioritas penelusuran berdasarkan anak pertama [Node sebelah kiri] ), maka penelusuran dilakukan terus melalui node anak pertama dari node anak pertama level sebelumnya hingga mencapai level terdalam. Setelah sampai di level terdalam, penelusuran akan kembali ke 1 level sebelumnya untuk menelusuri node anak kedua pada pohon biner [node sebelah kanan] lalu kembali ke langkah sebelumnya dengan menelusuri node anak pertama lagi sampai level terdalam dan seterusnya.

---
+++ Breadth First Search.py
@@ -0,0 +1,52 @@
+peta = {'A':set(['B']),
+        'B':set(['C','A']),
+        'C':set(['B','H','I','D']),
+        'D':set(['C','E','H','F']),
+        'E':set(['D']),
+        'F':set(['D','G']),
+        'G':set(['F','H']),
+        'H':set(['D','C','G','L']),
+        'I':set(['C','J','K']),
+        'J':set(['I']),
+        'K':set(['L','I']),
+        'L':set(['K','H'])}
+
+
+def bfs_lintasan_terpendek(graph, mulai, goal):
+    # mengecek semua node
+    explored = []
+    # mengecek semua jalur
+    queue = [[mulai]]
+
+    # kembali ke jalur apabila awal adalah tujuan
+    if mulai == goal:
+        return "Awal adalah Tujuan"
+
+    # perulangan sampai dengan semua jalur telah diperiksa
+    while queue:
+        # masukkan antrian paling depan ke variabel jalur
+        jalur = queue.pop(0)
+        # simpan node yang dipilih ke variabel state
+        node = jalur[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            # buat jalur baru dan
+            # masukan ke dalam queue
+            for neighbour in neighbours:
+                jalur_baru = list(jalur)
+                jalur_baru.append(neighbour)
+                queue.append(jalur_baru)
+                # kembali ke jalur apa bila cabang benar
+                if neighbour == goal:
+                    return jalur_baru
+
+            explored.append(node)
+
+    # dalam kasus ini tidak ada node yg diinputkan
+    return "Mohon maaf node yang kalian pilih tidak ada"
+
+
+awal = input("Masukan awal: ")
+tujuan = input("Masukan Akhir: ")
+
+print(bfs_lintasan_terpendek(peta, awal, tujuan))
